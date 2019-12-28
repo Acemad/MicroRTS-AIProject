5090703cf266a472ff36c2db3da2e192fe3199ef
Abdessamed Ouessai <ouessai.aes@gmail.com>
2019-12-28 16:26:12 +0100

    Reorganize experiments as own functions

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,7 @@
 import aes.uct.PlainUCT;
-import aes.uct.UCTRandomPruning;
-import aes.uct.emptyactions.PlainUCTNoEmptyAction;
 import aes.uct.emptyactions.UCTFixedInactionPruning;
 import aes.uct.emptyactions.UCTProbaInactionPruning;
-import ai.RandomBiasedAI;
-import ai.abstraction.LightRush;
-import ai.abstraction.WorkerRush;
 import ai.core.AI;
-import ai.core.ContinuingAI;
-import ai.mcts.informedmcts.InformedNaiveMCTS;
-import ai.mcts.naivemcts.NaiveMCTS;
-import ai.mcts.uct.DownsamplingUCT;
-import ai.mcts.uct.UCT;
-import ai.mcts.uct.UCTFirstPlayUrgency;
-import ai.mcts.uct.UCTUnitActions;
-import ai.montecarlo.lsi.LSI;
-import ai.portfolio.PortfolioAI;
-import ai.portfolio.portfoliogreedysearch.PGSAI;
-import ai.scv.SCV;
-import org.graphstream.stream.gephi.JSONSender;
 import rts.PhysicalGameState;
 import rts.units.UnitTypeTable;
 
@@ -41,19 +24,19 @@ public class Main {
     static AI maxPlayer = new UCTProbaInactionPruning(unitTypeTable, 0.6f), // Player at the top, Blue colored.
               minPlayer = new PlainUCT(unitTypeTable); // Player at the Bottom, Red colored.
 
-    public static void initialize(int mapLocationIndex, int maxCycles) throws Exception {
+    public static void initialize(AI maxPlayer, AI minPlayer, int mapLocationIndex, int maxCycles) throws Exception {
         physicalGameState = PhysicalGameState.load(mapLocations[mapLocationIndex], unitTypeTable);
-
-        experiment = new Experiments(maxPlayer, minPlayer, unitTypeTable, physicalGameState);
-        experiment.setMaxCycles(maxCycles);
+        experiment = new Experiments(maxPlayer, minPlayer, unitTypeTable, physicalGameState, maxCycles);
+//        experiment.setMaxCycles(maxCycles);
 
         System.out.println("Blue (Player 0 Max) : " + maxPlayer.getClass().getSimpleName());
         System.out.println("Red  (Player 1 Min) : " + minPlayer.getClass().getSimpleName());
     }
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Start Time : " + java.time.LocalTime.now());
-        testRPPParameters(MAP12X12, CYCLES12x12, 50, false,false);
+        System.out.println("Start Time : " + java.time.LocalDateTime.now());
+//        testAllRPPParametersVsUCT(MAP8X8, CYCLES8X8, 10, false,false);
+        testAllRFPParametersVsUCT(MAP12X12, CYCLES12x12, 50, false, false);
 //        experiment.runSingleMatch(false, true, true, true, false);
 
         // Send to Gephy.
@@ -66,24 +49,65 @@ public class Main {
         experiment.constructTree(((UCTProbaInactionPruning)maxPlayer).getTree());
         experiment.resetGraph();*/
 
-
 //        experiment.runMultipleMatches(20,false,true, false,false);
 //        experiment.runMultipleMatches(20,true, true, true, true);
 //        experiment.runMultipleMatchesSymmetric(10,true,false);
 
-        System.out.println("End Time : " + java.time.LocalTime.now());
+        System.out.println("End Time : " + java.time.LocalDateTime.now());
         System.exit(0);
     }
 
-    public static void testRPPParameters(int mapLocationIndex, int maxCycles, int totalNumberOfMatches,
-                                         boolean visualize, boolean printAIStats) throws Exception {
-        initialize(mapLocationIndex, maxCycles);
-        float[] parameters = new float[] {0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1f};
+    public static void testAllRFPParametersVsUCT(int mapLocationIndex, int maxCycles, int totalNumberOfMatches, boolean visualize,
+                                                 boolean printAIStats) throws Exception {
 
+        AI maxPlayer = new UCTFixedInactionPruning(unitTypeTable, 0), // Player at the top, Blue colored.
+           minPlayer = new PlainUCT(unitTypeTable); // Player at the Bottom, Red colored.
+
+        int[] parameters = new int[] {0, 1, 5, 10, 50, 100, 500, 1000};
+        initialize(maxPlayer, minPlayer, mapLocationIndex, maxCycles);
+        for (int parameter : parameters) {
+            ((UCTFixedInactionPruning)experiment.getMaxPlayer()).setAllowedInactions(parameter);
+//            ((UCTProbaInactionPruning)maxPlayer).setInactionAllowProbability(parameter);
+            System.out.println("** Starting Experiment with allowed inactions = " + parameter);
+            experiment.runMultipleMatchesSymmetric(totalNumberOfMatches, visualize, printAIStats);
+        }
+    }
+
+    public static void testAllRPPParametersVsUCT(int mapLocationIndex, int maxCycles, int totalNumberOfMatches, boolean visualize,
+                                                 boolean printAIStats) throws Exception {
+
+        AI maxPlayer = new UCTProbaInactionPruning(unitTypeTable, 0f), // Player at the top, Blue colored.
+           minPlayer = new PlainUCT(unitTypeTable); // Player at the Bottom, Red colored.
+
+        float[] parameters = new float[] {0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1f};
+        initialize(maxPlayer, minPlayer, mapLocationIndex, maxCycles);
         for (float parameter : parameters) {
-            ((UCTProbaInactionPruning)maxPlayer).setInactionAllowProbability(parameter);
+            ((UCTProbaInactionPruning)experiment.getMaxPlayer()).setInactionAllowProbability(parameter);
+//            ((UCTProbaInactionPruning)maxPlayer).setInactionAllowProbability(parameter);
             System.out.println("** Starting Experiment with p = " + parameter);
             experiment.runMultipleMatchesSymmetric(totalNumberOfMatches, visualize, printAIStats);
         }
     }
+
+    public static void testRPPParameterVsUCT(int mapLocationIndex, int maxCycles, int totalNumberOfMatches, float parameter,
+                                             int numberOfTests, boolean visualize, boolean printAIStats) throws Exception {
+
+        AI maxPlayer = new UCTProbaInactionPruning(unitTypeTable, parameter),
+           minPlayer = new PlainUCT(unitTypeTable);
+
+        initialize(maxPlayer, minPlayer, mapLocationIndex, maxCycles);
+
+        for (int testId = 0; testId < numberOfTests; testId++) {
+            System.out.println("** Starting experiment " + testId +
+                    " with p = " + ((UCTProbaInactionPruning)experiment.getMaxPlayer()).getInactionAllowProbability());
+            experiment.runMultipleMatchesSymmetric(totalNumberOfMatches, visualize, printAIStats);
+        }
+    }
+
+
+
+
+
+
+
 }
